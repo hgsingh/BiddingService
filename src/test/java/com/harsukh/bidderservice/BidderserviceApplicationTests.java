e422ef0aa6a604ecc5a5464cf38f45880f3d8882
Harsukh Singh <singhharsukh@gmail.com>
2019-03-07 01:22:40 -0500

    basic tests

--- src/test/java/com/harsukh/bidderservice/BidderserviceApplicationTests.java
+++ src/test/java/com/harsukh/bidderservice/BidderserviceApplicationTests.java
@@ -1,16 +1,81 @@
 package com.harsukh.bidderservice;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.setup.DefaultMockMvcBuilder;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.util.NestedServletException;
+
+import java.nio.charset.Charset;
+import java.util.Arrays;
+
+import static org.junit.Assert.assertNotNull;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = BidderserviceApplication.class)
+@WebAppConfiguration
 public class BidderserviceApplicationTests {
 
 	@Test
 	public void contextLoads() {
 	}
 
+	private MediaType contentType = new MediaType(MediaType.APPLICATION_JSON.getType(),
+			MediaType.APPLICATION_JSON.getSubtype(),
+			Charset.forName("utf8"));
+
+	private MockMvc mockMvc;
+
+	private HttpMessageConverter mappingJackson2HttpMessageConverter;
+
+
+	private Bid bid;
+
+	@Autowired
+	private AuctionItemRepository auctionItemRepository;
+
+	@Autowired
+	private BiddingRepository biddingRepository;
+
+	@Autowired
+	private WebApplicationContext webApplicationContext;
+
+	@Autowired
+	void setConverters(HttpMessageConverter<?>[] converters) {
+
+		this.mappingJackson2HttpMessageConverter = Arrays.asList(converters).stream()
+				.filter(hmc -> hmc instanceof MappingJackson2HttpMessageConverter)
+				.findAny()
+				.orElse(null);
+
+		assertNotNull("the JSON message converter must not be null",
+				this.mappingJackson2HttpMessageConverter);
+	}
+
+	@Before
+	public void setup() throws Exception {
+		this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+		this.auctionItemRepository.deleteAll();
+		this.biddingRepository.deleteAll();
+	}
+
+
+	@Test(expected = NestedServletException.class)
+	public void orderNotFound() throws Exception {
+		mockMvc.perform(get("/auctionItems"))
+				.andExpect(status().is5xxServerError());
+	}
 }
